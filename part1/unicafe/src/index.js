f6b41827d6c38a33e032f3ebd0484321a0c1a906
Cesar Jimenez <cijgr@hotmail.com>
2019-09-27 13:19:57 -0500

    Exercise 1.10: unicafe step 5 COMPLETED

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,6 +4,12 @@ import "./index.css";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const Statistic = ({ text, value }) => (
+  <p>
+    {text}: {value}
+  </p>
+);
+
 const Statistics = props => {
   const { good, neutral, bad } = props;
   const all = good + neutral + bad;
@@ -21,12 +27,12 @@ const Statistics = props => {
   return (
     <div>
       <h2>statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>average: {average}</p>
-      <p>positive: {positive}%</p>
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={all} />
+      <Statistic text="average" value={average} />
+      <Statistic text="positive" value={positive} />
     </div>
   );
 };
