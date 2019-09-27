25effec30f59a228b9495090fabc6a3df7c721c7
Cesar Jimenez <cijgr@hotmail.com>
2019-09-27 13:03:33 -0500

    Exercise 1.8: unicafe step 3 COMPLETED

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,15 +4,30 @@ import "./index.css";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const Statistics = props => {
+  const { good, neutral, bad } = props;
+  const all = good + neutral + bad;
+  const average = all ? (good * 1 + bad * -1) / all : 0;
+  const positive = all ? (good / all) * 100 : 0;
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const all = good + neutral + bad;
-  const average = all ? (good * 1 + bad * -1) / all : 0;
-  const positive = all ? (good / all) * 100 : 0;
 
   const handleGood = () => {
     setGood(good + 1);
@@ -30,15 +45,7 @@ const App = () => {
       <Button onClick={handleGood} text="good" />
       <Button onClick={handleNeutral} text="neutral" />
       <Button onClick={handleBad} text="bad" />
-      <h2>statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>average: {average}</p>
-      <p>positive: {positive}%</p>
-      {/* <Button onClick={handleNeutral} text={neutral} />
-      <Button onClick={handleBad} text={bad} /> */}
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
