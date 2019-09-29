e3c4c5dd5c41177c4060efc3dcff8bcfb56dc75a
Cesar Jimenez <cijgr@hotmail.com>
2019-09-29 16:18:29 -0500

    Part2 - exercise 2.9 step4 COMPLETED

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "12-12-1234567", id: "Arto Hellas" }
+    { name: "Arto Hellas", phone: "040-123456" },
+    { name: "Ada Lovelace", phone: "39-44-5323523" },
+    { name: "Dan Abramov", phone: "12-43-234345" },
+    { name: "Mary Poppendieck", phone: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [newSearch, setNewSearch] = useState("");
 
   const addPerson = event => {
     event.preventDefault();
 
     const personObject = {
       name: newName,
-      phone: newPhone,
-      id: newName
+      phone: newPhone
     };
 
     const nameExists = persons.find(personObject => {
@@ -37,16 +40,32 @@ const App = () => {
     setNewPhone(event.target.value);
   };
 
-  const rows = () =>
-    persons.map(person => (
-      <li key={person.id}>
-        {person.name} / {person.phone}
-      </li>
-    ));
+  const handleSearchChange = event => {
+    setNewSearch(event.target.value);
+  };
+
+  const rows = () => {
+    let filteredPersons = persons.filter(person => {
+      return person.name.toUpperCase().indexOf(newSearch.toUpperCase()) !== -1;
+    });
+
+    return filteredPersons.map(person => {
+      return (
+        <li key={person.name}>
+          {person.name} / {person.phone}
+        </li>
+      );
+    });
+  };
 
   return (
     <div>
       <h1>PhoneBook</h1>
+      <div>
+        filter shown with:{" "}
+        <input id="search" value={newSearch} onChange={handleSearchChange} />
+      </div>
+      <br />
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
