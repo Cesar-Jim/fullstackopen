e87f85be159ffafb9808bcbb557ba1c7ab3995dc
Cesar Jimenez <cijgr@hotmail.com>
2019-10-02 21:37:33 -0500

    Theory of part2e finished, ready to begin exercises

--- part2/src/components/Note.js
+++ part2/src/components/Note.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Note = ({ note, toggleImportance }) => {
-  const label = note.important ? 'make NOT important' : 'make important';
+  const label = note.important ? "make NOT important" : "make important";
 
   return (
-    <li>
-    {note.content}
-    <button onClick={toggleImportance}>{label}</button>
+    <li className="note">
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
     </li>
-  )
+  );
 };
 
 export default Note;
