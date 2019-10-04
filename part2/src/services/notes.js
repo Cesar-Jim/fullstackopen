032bb13b45b11b95b187f4eef7e8acd243fe5768
Cesar Jimenez <cijgr@hotmail.com>
2019-10-04 12:36:14 -0500

    Changed once again baseUrl to a relative one. Deploying FE

--- part2/src/services/notes.js
+++ part2/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://polar-earth-75450.herokuapp.com/notes";
+const baseUrl = "/notes";
 
 // The following functions return the promises returned by the axios methods:
 
