879149033651421bf5a3367dc3e0ce924c3cf01a
Cesar Jimenez <cijgr@hotmail.com>
2019-10-04 14:11:24 -0500

    Did some changes on the base url and package json to make things work with the backend

--- part2/src/services/notes.js
+++ part2/src/services/notes.js
@@ -1,18 +1,12 @@
 import axios from "axios";
 
-const baseUrl = "/notes";
+const baseUrl = "/api/notes";
 
 // The following functions return the promises returned by the axios methods:
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  const nonExisting = {
-    id: 1000,
-    content: "This note is not saved to the server",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  };
-  return request.then(response => response.data.concat(nonExisting));
+  return request.then(response => response.data);
 };
 
 const create = newObject => {
