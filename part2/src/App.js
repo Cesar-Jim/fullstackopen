a0b10db0fad15a9ec6df52bab2d0d49e2a096853
Cesar Jimenez <cijgr@hotmail.com>
2019-11-11 10:19:48 -0600

    theory of part 5b COMPLETED

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,20 +1,63 @@
-import React, { useState, useEffect } from "react";
-import Note from "./components/Note";
-import noteService from "./services/notes";
-import Notification from "./components/Notification";
-import Footer from "./components/Footer";
+import React, { useState, useEffect } from 'react';
+import Note from './components/Note';
+import noteService from './services/notes';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
+import loginService from './services/login';
+import LoginForm from './components/LoginForm';
+import Togglable from './components/Togglable';
+import NoteForm from './components/NoteForm';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
+  const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [loginVisible, setLoginVisible] = useState(false);
+
+  const noteFormRef = React.createRef();
 
   useEffect(() => {
     noteService.getAll().then(initialNotes => setNotes(initialNotes));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser');
+
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
+
   const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
+  const handleLogin = async event => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user));
+
+      noteService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
 
   const rows = () =>
     notesToShow.map(note => (
@@ -25,22 +68,53 @@ const App = () => {
       />
     ));
 
+  const loginForm = () => {
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' };
+    const showWhenVisible = { display: loginVisible ? '' : 'none' };
+
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>Log in</button>
+        </div>
+        <div style={showWhenVisible}>
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+          <button onClick={() => setLoginVisible(false)}>cancel</button>
+        </div>
+      </div>
+    );
+  };
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange}></input>
+      <button type='submit'>save</button>
+    </form>
+  );
+
   const handleNoteChange = event => {
     setNewNote(event.target.value);
   };
 
   const addNote = event => {
     event.preventDefault();
+    noteFormRef.current.toggleVisibility();
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1
+      id: notes.length + 1,
     };
 
     noteService.create(noteObject).then(data => {
       setNotes(notes.concat(data));
-      setNewNote("");
+      setNewNote('');
     });
   };
 
@@ -55,7 +129,7 @@ const App = () => {
       })
       .catch(error => {
         setErrorMessage(
-          `Note: "${note.content}" was already removed from server.`
+          `Note: "${note.content}" was already removed from server.`,
         );
         setTimeout(() => {
           setErrorMessage(null);
@@ -67,17 +141,32 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+
       <Notification message={errorMessage} />
+
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged in</p>
+          <Togglable buttonLabel='new note' ref={noteFormRef}>
+            <NoteForm
+              onSubmit={addNote}
+              value={newNote}
+              handleChange={handleNoteChange}
+            />
+          </Togglable>
+        </div>
+      )}
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important" : "all"}
+          {' '}
+          show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>{rows()}</ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+
       <Footer />
     </div>
   );
--- part4/blog/src/services/blogs.js
+++ part4/blog/src/services/blogs.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/blogs';
+const baseUrl = '/api/blogs';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
